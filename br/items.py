7350a30e6b89072a28d162272d3700b6058c8e16
badn3wz <lukaszburdzy@gmail.com>
2015-07-10 10:56:41 +0200

    initial

---
+++ br/items.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class GameItem(scrapy.Item):
+    date = scrapy.Field()
+    team_name = scrapy.Field() #wyciagnac wczesniej z url
+    opponent_name = scrapy.Field()
+    at_home = scrapy.Field() # wyciagnac z @
+    game_won = scrapy.Field() #boolean na podstawie tm_pts - opp_pts
+    team_points = scrapy.Field()
+    opponent_points = scrapy.Field()
+    field_goals = scrapy.Field()
+    field_goal_attempts = scrapy.Field()
+    field_goal_percentage = scrapy.Field()
+    three_pointers = scrapy.Field()
+    three_point_atempts = scrapy.Field()
+    three_point_percentage = scrapy.Field()
+    free_throws = scrapy.Field()
+    free_throw_attempts = scrapy.Field()
+    free_throw_percentage = scrapy.Field()
+    offensive_rebounds = scrapy.Field()
+    defensive_rebounds = scrapy.Field() # wyliczyc z trb-orb
+    total_rebounds = scrapy.Field()
+    assists = scrapy.Field()
+    steals = scrapy.Field()
+    blocks = scrapy.Field()
+    turnovers = scrapy.Field()
+    personal_fouls = scrapy.Field()
---
+++ br/spiders/br_spider.py
@@ -0,0 +1,83 @@
+
+
+
+import scrapy
+
+from br.items import BrItem
+
+class GamesSpider(scrapy.Spider):
+    name = "gamespider"
+    allowed_domains = ["basketball-reference.com"]
+    start_urls = [
+    "http://www.basketball-reference.com/teams/BOS/2010/gamelog"
+    ]
+
+    def parse(self, response):
+        item = BrItem()
+        for sel in response.xpath('//table[contains(@id, "tgl_basic")]/tbody/tr[contains(@id, "tgl_basic")]'):
+
+
+            #misc data
+            item['date'] = sel.xpath('td[3]/a/text()').extract()
+            item['opponent_name'] = sel.xpath('td[5]/a/text()').extract()
+            item['at_home'] = sel.xpath('td[4]/text()').extract()
+            item['game_won'] = sel.xpath('td[6]/text()').extract()
+            item['team_points'] = sel.xpath('td[7]/text()').extract()
+            item['opponent_points'] = sel.xpath('td[8]/text()').extract()
+
+            #shooting
+            item['field_goals'] = sel.xpath('td[9]/text()').extract()
+            item['field_goal_attempts'] = sel.xpath('td[10]/text()').extract()
+            item['field_goal_percentage'] = sel.xpath('td[11]/text()').extract()
+            item['three_pointers'] = sel.xpath('td[12]/text()').extract()
+            item['three_point_atempts'] = sel.xpath('td[13]/text()').extract()
+            item['three_point_percentage'] = sel.xpath('td[14]/text()').extract()
+            item['free_throws'] = sel.xpath('td[15]/text()').extract()
+            item['free_throw_attempts'] = sel.xpath('td[16]/text()').extract()
+            item['free_throw_percentage'] = sel.xpath('td[17]/text()').extract()
+
+            #offense
+            item['offensive_rebounds'] = sel.xpath('td[18]/text()').extract()
+            item['assists'] = sel.xpath('td[20]/text()').extract()
+            item['turnovers'] = sel.xpath('td[23]/text()').extract()
+
+
+
+            #defence
+            item['steals'] = sel.xpath('td[21]/text()').extract()
+            item['blocks'] = sel.xpath('td[22]/text()').extract()
+            item['personal_fouls'] = sel.xpath('td[24]/text()').extract()
+
+
+            item['total_rebounds'] = sel.xpath('td[19]/text()').extract()
+
+
+            yield item
+
+            # yield scrapy.Request(response.urljoin(url), self)#.parse_titles)
+
+
+
+
+
+'''
+
+'''
+
+
+
+
+
+
+'''
+/teams/
+response.xpath('//*[@id="active"]/tbody//tr[@class="full_table"]/td[1]/a/@href').extract()
+
+
+/teams/aaa/
+response.xpath('//*[@id="ATL"]/tbody//tr/td[1]/a/@href').extract()
+
+zrobic @text()=available
+
+//*[@id="info_box"]/div[4]/ul/li[6]/ul/li[1]/a
+'''
